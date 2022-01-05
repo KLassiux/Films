2bae5ed04f86012825d1848fec77a2176c9b4561
Pierre Barrere <barrerepierre0705@gmail.com>
2022-01-05 14:39:36 +0100

    debout

---
+++ Movies/Movies/Views/Index.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Movies.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Movies.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Index : ContentPage
+    {
+        public Index()
+        {
+            InitializeComponent();
+            BindingContext = new MoviesViewModel();
+        }
+    }
+}
